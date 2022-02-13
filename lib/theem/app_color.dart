2081bf838d6c025dd23971ff197d0427342b50fb
Danushan <it20051648@my.sliit.lk>
2022-02-14 00:21:25 +0530

    light theem

--- lib/theem/app_color.dart
+++ lib/theem/app_color.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class AppColor {
   static Color textColor = const Color(0xff9c9c9d);
   static Color textDarkColor = const Color(0xffffffff);
-  
 
+  static Color bodyColor = const Color(0xffffffff);
+  static Color bodyColorDark = const Color(0xff0e0e0f);
+
+  static Color buttonbackgroundcolor = const Color(0xfff7f7f7);
+  static Color buttonbackgroundDarkcolor = const Color(0xff121212);
 }
