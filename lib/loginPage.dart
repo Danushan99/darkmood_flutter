2e8a20e41a89e7581bdb857548563f7cea3b36fd
Danushan <it20051648@my.sliit.lk>
2022-02-14 20:14:22 +0530

    finsih

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -12,12 +12,12 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    final Size = MediaQuery.of(context).size;
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
         body: SingleChildScrollView(
       child: Container(
-        width: Size.width,
-        height: Size.height,
+        width: size.width,
+        height: size.height,
         padding: const EdgeInsets.only(
           left: 20,
           right: 20,
@@ -25,6 +25,7 @@ class _LoginPageState extends State<LoginPage> {
           bottom: 80,
         ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
@@ -32,12 +33,13 @@ class _LoginPageState extends State<LoginPage> {
               style: Theme.of(context).textTheme.headline1,
             ),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: const [
                     Image(
-                      width: 55,
+                      width: 43,
                       image: AssetImage("asserts/images/google.png"),
                     ),
                     SizedBox(
@@ -48,9 +50,68 @@ class _LoginPageState extends State<LoginPage> {
                       image: AssetImage("asserts/images/fb.png"),
                     ),
                   ],
-                )
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColorLight,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Email Bro", border: InputBorder.none),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColorLight,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter Password Bro",
+                        border: InputBorder.none),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  "Forgot Password",
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
               ],
-            )
+            ),
+            Column(
+              children: [
+                RaisedButton(
+                  padding: EdgeInsets.all(10),
+                  onPressed: () => {},
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text("Create Account",
+                    style: Theme.of(context).textTheme.bodyText1),
+              ],
+            ),
           ],
         ),
       ),
